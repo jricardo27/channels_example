c018316f6941ff460812d9de8d8dfeefe6039f62
Ricardo Perez <ricardo.perez@wspdigital.com>
2017-04-18 10:08:42 +1000

    Add Django Channels to the project

--- channels_example/test.py
+++ channels_example/test.py
@@ -1,10 +1,13 @@
+from channels.test.liveserver import ChannelLiveServerTestCase
 from django.test import override_settings
 
-from aloe_django import TestCase as AloeTestCase
+from aloe.testclass import TestCase as AloeTestCase
 
 
 @override_settings(DEBUG=True)
-class TestCase(AloeTestCase):
-    """Base test class for Django Gherkin tests with DEBUG enabled."""
+class TestCase(ChannelLiveServerTestCase, AloeTestCase):
+    """
+    Base test class for Django Gherkin tests in conjunction with Channels.
+    """
 
     pass
