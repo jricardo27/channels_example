86b5a827effe7f0e28e0582ee33af74c40ff633f
Ricardo Perez <ricardo.perez@wspdigital.com>
2017-04-24 17:01:45 +1000

    Close DB connections before running Worker and Daphne processes

--- channels_example/test.py
+++ channels_example/test.py
@@ -1,13 +1,18 @@
 from channels.test.liveserver import ChannelLiveServerTestCase
+from django.db import connections
 from django.test import override_settings
 
 from aloe.testclass import TestCase as AloeTestCase
 
 
-@override_settings(DEBUG=True)
+@override_settings(DEBUG=True, ALLOWED_HOSTS=['*'])
 class TestCase(ChannelLiveServerTestCase, AloeTestCase):
     """
     Base test class for Django Gherkin tests in conjunction with Channels.
     """
 
-    pass
+    def _pre_setup(self):
+        # Close all DB connections before running Worker and Daphne processes.
+        connections.close_all()
+
+        super()._pre_setup()
